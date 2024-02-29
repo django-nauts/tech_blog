1d0a54f37c130620e0952752c72b4f06de5933c7
djangonauts <djangonauts@gmail.com>
2024-03-01 00:26:06 +0330

    Add pagination

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import (
-	render, redirect, get_object_or_404
+    render, redirect, get_object_or_404
 )
+from django.core.paginator import Paginator
 
 from blog.models import Post
 from blog.forms import PostForm, UpdatePostForm
@@ -8,108 +9,118 @@ from blog.forms import PostForm, UpdatePostForm
 
 # Show specific category
 def blog_category(request, category):
-	posts = Post.objects.filter(
-		categories__name__contains=category
-	).order_by('-created')
-	
-	context = {
-		'category': category,
-		'posts': posts,
-	}
-	return render(request, 'blog/tech-category.html', context)
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by('-created')
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    context = {
+        'category': category,
+        'posts': posts,
+		'page_obj': page_obj,
+    }
+    return render(request, 'blog/tech-category.html', context)
 
 
 # Create a new post
 def blog_create(request):
 
-	if request.method == 'POST':
-		form = PostForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
 
-		if form.is_valid():
-			form.save()
-			return redirect('blog_index')
-	else:
-		form = PostForm()
-		
-	context = {
-		'form': form,
-	}
-	return render(request, 'blog/create.html', context)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_index')
+    else:
+        form = PostForm()
+        
+    context = {
+        'form': form,
+    }
+    return render(request, 'blog/create.html', context)
 
 
 # Home page
 def blog_index(request):
 
-	if request.method == 'GET':
-		posts = Post.published_posts.all().order_by('-created')
-		context = {
-			'posts': posts,
-		}
-		return render(request, 'blog/tech-index.html', context)
+    if request.method == 'GET':
+        posts = Post.published_posts.all().order_by('-created')
+        paginator = Paginator(posts, 10)
+
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        context = {
+            'posts': posts,
+            'page_obj': page_obj,
+        }
+        return render(request, 'blog/tech-index.html', context)
 
 
 # Show each post
 def blog_detail(request, slug):
-	post = Post.objects.get(slug=slug)
-	context = {
-		'post': post,
-	}
-	return render(request, 'blog/tech-single.html', context)
+    post = Post.objects.get(slug=slug)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/tech-single.html', context)
 
 
 # Update post
 def blog_update(request, slug):
-	post = get_object_or_404(Post, slug=slug)
+    post = get_object_or_404(Post, slug=slug)
 
-	if request.method == 'POST':
-		form = UpdatePostForm(request.POST, request.FILES, instance=post)
-		if form.is_valid():
-			form.save()
-			return redirect('blog_index')
-	else:
-		form = UpdatePostForm(instance=post)
-	
-	context = {
-		'post': post,
-		'form': form,
-	}
-	return render(request, 'blog/update.html', context)
+    if request.method == 'POST':
+        form = UpdatePostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_index')
+    else:
+        form = UpdatePostForm(instance=post)
+    
+    context = {
+        'post': post,
+        'form': form,
+    }
+    return render(request, 'blog/update.html', context)
 
 
 # Delete post
 def blog_delete(request, slug):
-	context = {}
-	post = get_object_or_404(Post, slug=slug)
+    context = {}
+    post = get_object_or_404(Post, slug=slug)
 
-	if request.method == 'POST':
-		post.delete()
-		return redirect('blog_index')
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog_index')
 
-	return render(request, 'blog/delete.html', context)
+    return render(request, 'blog/delete.html', context)
 
 
 # Show author posts and information
 def blog_author(request):
-	context = {}
-	return render(request, 'blog/tech-author.html', context)
+    context = {}
+    return render(request, 'blog/tech-author.html', context)
 
 
 def blog_category_01(request):
-	context = {}
-	return render(request, 'blog/tech-category-01.html', context)
+    context = {}
+    return render(request, 'blog/tech-category-01.html', context)
 
 
 def blog_category_02(request):
-	context = {}
-	return render(request, 'blog/tech-category-02.html', context)
+    context = {}
+    return render(request, 'blog/tech-category-02.html', context)
 
 
 def blog_category_03(request):
-	context = {}
-	return render(request, 'blog/tech-category-03.html', context)
+    context = {}
+    return render(request, 'blog/tech-category-03.html', context)
 
 
 # Contact us
 def blog_contact(request):
-	context = {}
-	return render(request, 'blog/tech-contact.html', context)
+    context = {}
+    return render(request, 'blog/tech-contact.html', context)
