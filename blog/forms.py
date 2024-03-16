50d7dc1affb3af67b4abd87702501661d6b168bc
ventuno-21 <yasna.pakdel@outlook.com>
2024-03-16 09:33:23 -0700

    Add tag field to Update & Create forms, which I deleted by mistake

--- blog/forms.py
+++ blog/forms.py
@@ -12,7 +12,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'body', 'cover']
+        fields = ['title', 'body', 'cover','tags']
         widgets = {
             'title': forms.TextInput( attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control' }),
@@ -30,7 +30,7 @@ class UpdatePostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'body', 'cover',]
+        fields = ['title', 'body', 'cover','tags']
         widgets = {
             'title': forms.TextInput( attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control' }),
